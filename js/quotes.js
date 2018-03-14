d797724158508f117862702c8bdb689dce6a7777
Chris Holder <chrispholder@gmail.com>
2018-03-13 20:16:23 -0400

    Populate quotes array with real data; add more

--- js/quotes.js
+++ js/quotes.js
@@ -1,28 +1,74 @@
 const quotes = [
   {
-    quote: 'Bacon ipsum dolor amet ham drumstick flank.',
-    source: 'Meatloaf kevin',
-    citation: 'Shankle rump pancetta ground round',
-    year: '1987',
-    tags: ['bacon', 'ham', 'delicious'],
+    quote: 'Our deeds determine us, as much as we determine our deeds.',
+    source: 'Marian Evans',
   },
   {
-    quote: 'Drumstick buffalo turducken, pancetta jowl tail capicola biltong doner.',
-    source: 'Chuck pork',
-    tags: ['bacon', 'ham'],
+    quote:
+      'The probability that we may fail in the struggle ought not to deter us from the support of a cause we believe to be just.',
+    source: 'Abraham Lincoln',
   },
   {
-    quote: 'Pancetta strip steak corned beef landjaeger shoulder shankle prosciutto.',
-    source: 'Turkey kielbasa',
-    citation: ' Cupim landjaeger tenderloin',
+    quote:
+      "The grass is always greener once you don't have to mow a lawn anymore.",
+    source: 'Randy K. Milholland',
+    citation: 'Something Positive',
+    year: '2013',
+    tags: ['Maturity'],
   },
   {
-    quote: 'Meatloaf flank pork belly capicola, chicken porchetta picanha frankfurter sausage filet mignon.',
-    source: 'Pork beef',
-    year: '1870',
+    quote: 'Virtue is choked with foul ambition.',
+    source: 'William Shakespeare',
+    citation: 'Henry VI',
+    tags: ['Ambition'],
   },
   {
-    quote: 'Jerky jowl meatball shank.',
-    source: 'Flank turkey',
+    quote:
+      'In order to preserve your self-respect, it is sometimes necessary to lie and cheat.',
+    source: 'Robert Byrne',
+  },
+  {
+    quote:
+      "The grass is always greener once you don't have to mow a lawn anymore.",
+    source: 'Oscar Wilde',
+    citation: 'An Ideal Husband',
+    year: '1893',
+  },
+  {
+    quote:
+      "We laugh a lot. That's for sure. Sure beats the alternative, doesn't it?",
+    source: 'Betty White',
+    citation: 'Mark Twain Prize',
+    year: '2010',
+    tags: ['Laughter'],
+  },
+  {
+    quote: "I don't really trust a sane person.",
+    source: 'Lyle Alzado',
+    tags: ['Sanity'],
+  },
+  {
+    quote:
+      'A person who forgoes the use of his symbolic skills is never really free.',
+    source: 'Mihaly Csikszentmihalyi',
+    citation: 'Flow: The Psychology of Optimal Experience',
+    year: '1990',
+  },
+  {
+    quote:
+      "We can't all be heroes because somebody has to sit on the curb and clap as they go by.",
+    source: 'Will Rogers',
+    tags: ['Heroes'],
+  },
+  {
+    quote:
+      'We may not imagine how our lives could be more frustrating and complex--but Congress can.',
+    source: 'Cullen Hightower',
+  },
+  {
+    quote:
+      'History is indeed little more than the register of the crimes, follies and misfortunes of mankind.',
+    source: 'Edward Gibbon',
+    tags: ['History', 'Crime'],
   },
 ];
